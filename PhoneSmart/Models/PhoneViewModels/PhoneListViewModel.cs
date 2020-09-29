323315075407224d6d2b21358183211c3663c16d
Ashon Woodbury <a.woodbury01@gmail.com>
2020-09-29 15:30:38 -0400

    restored lost data

---
+++ PhoneSmart/Models/PhoneViewModels/PhoneListViewModel.cs
@@ -0,0 +1,9 @@
+﻿using System.Collections.Generic;
+
+namespace PhoneSmart.Models.PhoneViewModels
+{
+    public class PhoneListViewModel
+    {
+        public IEnumerable<Phone> Phones { get; set; }
+    }
+}
