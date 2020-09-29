48bd9bff4bab1400f8f857f5c7b75ad953b211f6
Ashon Woodbury <a.woodbury01@gmail.com>
2020-09-29 10:52:28 -0400

    initial progress after day one

---
+++ PhoneSmart/Data/ApplicationDbContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PhoneSmart.Models;
+
+namespace PhoneSmart.Data
+{
+    public class ApplicationDbContext : IdentityDbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<PhoneModel> PhoneModel { get; set; }
+        public DbSet<Phone> Phone { get; set; }
+        public DbSet<ApplicationUser> ApplicationUsers { get; set; }
+
+    }
+}
