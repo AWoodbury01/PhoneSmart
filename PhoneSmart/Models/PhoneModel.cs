39f0fbd7028eb7a0ac2c6f84726745c67de497fc
Ashon Woodbury <a.woodbury01@gmail.com>
2020-10-15 14:27:54 -0400

    added labels to phone model. updated design of edit page

--- PhoneSmart/Models/PhoneModel.cs
+++ PhoneSmart/Models/PhoneModel.cs
@@ -1,23 +1,46 @@
-﻿namespace PhoneSmart.Models
+﻿using System.ComponentModel;
+
+namespace PhoneSmart.Models
 {
     public class PhoneModel
     {
         public int PhoneModelId { get; set; }
         public string Manufacturer { get; set; }
         public string Model { get; set; }
+
+        [DisplayName("Operating System")]
         public string OS { get; set; }
         public string Ram { get; set; }
+
+        [DisplayName("Display Type")]
         public string DisplayType { get; set; }
+
+        [DisplayName("Display Size")]
         public double DisplaySize { get; set; }
+
+        [DisplayName("Refresh Rate")]
         public string RefreshRate { get; set; }
+
         public string Processor { get; set; }
+
+        [DisplayName("Main Camera")]
         public string MainCam { get; set; }
+
+        [DisplayName("Secondary Camera")]
         public string SecondaryCam { get; set; }
+
+        [DisplayName("Front Camera")]
         public string FrontCam { get; set; }
         public string Battery { get; set; }
         public string Security { get; set; }
+
+        [DisplayName("Wireless Charging")]
         public bool isWirelessCharge { get; set; }
+
+        [DisplayName("Water Resistance")]
         public bool isWaterResist { get; set; }
+
+
         public string PhoneURL { get; set; }
     }
 }
